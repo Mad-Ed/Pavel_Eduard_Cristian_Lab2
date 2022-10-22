68299715556ad968cc0523d68054307be1aedf21
victo <victo@DESKTOP-00RKQ12>
2022-10-22 19:36:26 +0300

    Add project files.

---
+++ Data/Pop_Darius_Victor_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Darius_Victor_Lab2.Models;
+
+namespace Pop_Darius_Victor_Lab2.Data
+{
+    public class Pop_Darius_Victor_Lab2Context : DbContext
+    {
+        public Pop_Darius_Victor_Lab2Context (DbContextOptions<Pop_Darius_Victor_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Darius_Victor_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Pop_Darius_Victor_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Pop_Darius_Victor_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Migrations/20221022162202_LastName.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pop_Darius_Victor_Lab2.Migrations
+{
+    public partial class LastName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
